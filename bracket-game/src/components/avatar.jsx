e2bfd6dc62ee5900f0751f74dd02ced2c43eb6e9
Cole <cbrossar@cisco.com>
2020-06-28 13:30:22 -0400

    remove unnecessary avatar  js files

--- bracket-game/src/components/avatar.jsx
+++ bracket-game/src/components/avatar.jsx
@@ -1,6 +1,21 @@
 import React from "react";
 import { Person } from "@material-ui/icons";
-import avatars from "../js/avatars";
+import { AcUnit, Adb, BeachAccess } from "@material-ui/icons";
+
+const avatars = {
+  ac_unit: <AcUnit />,
+  adb: <Adb />,
+  beach_access: <BeachAccess />,
+};
+
+function getRandomAvatar() {
+  console.log(avatars);
+  const keys = Object.keys(avatars);
+  const pos = Math.floor(Math.random() * keys.length);
+  return keys[pos];
+}
+
+export { avatars, getRandomAvatar };
 
 function PlayerAvatar(props) {
   if (props.avatar in avatars) {
--- bracket-game/src/components/join-game.jsx
+++ bracket-game/src/components/join-game.jsx
@@ -3,7 +3,7 @@ import { makeStyles } from "@material-ui/core/styles";
 import NameDialog from "./name-dialog";
 import { Typography, TextField, Button, Container } from "@material-ui/core";
 import db from "../index";
-import getRandomAvatar from "../js/random-avatar";
+import { getRandomAvatar } from "./avatar";
 
 const useStyles = makeStyles((theme) => ({
   root: {
